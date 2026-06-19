301f2d1ff496b2573fbc28638aed555f5f1802b2
fatboy13-Adrian <birdbird_dog@hotmail.sg>
2026-06-19 17:04:57 +0800

    my first commit

---
+++ src/main/java/com/app/personalfinanacetracker/PersonalfinanacetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.app.personalfinanacetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalfinanacetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalfinanacetrackerApplication.class, args);
+	}
+
+}
